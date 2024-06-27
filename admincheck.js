6e589759953c7fff3403f1e78873e60ccf4f8e42
marouane216 <uanemaro216@gmail.com>
2024-06-27 22:10:40 +0100

    update

--- admincheck.js
+++ admincheck.js
@@ -2,14 +2,17 @@ const email = "inbox@apexweb.live"
 const password = "youcode.ma"
 
 var emailArray = email.split("@")
-var prefix = emailArray[0]
+
+    var prefix = emailArray[0] // box
+
 var domainArray = emailArray[1].split(".")
-var domain = domainArray[0]
-var extension = domainArray[1]
+
+    var domain = domainArray[0] // exweb
+    var extension = domainArray[1] // ve
 
 var passwordArray = password.split(".")
-var prefixPassword = passwordArray[0]
-var domainPassword = passwordArray[1]
 
-const token = prefix[0] + domainArray[0] + extension[0] + prefixPassword[0] + domainPassword[0] + "¤"
+    var prefixPassword = passwordArray[0] // ucode
+    var domainPassword = passwordArray[1]
+const token = prefix[0] + domain[0] + extension[0] + prefixPassword[0] + domainPassword[0] + "¤" + prefix[1] + domain[1] + extension[1] + prefixPassword[1] + domainPassword[1] + "¤" 
 console.log(token);
